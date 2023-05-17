3b9472e348864a7f1848f9d04a197f202ff21f47
Natalia <nataliaouteiral@gmail.com>
2023-05-17 19:43:40 +0200

    changed pic

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -8,19 +8,19 @@ const About = () => {
       <Header />
       <div className="main">
         <section className="about__section">
-          <h2 className="section__subtitle--2">
-            Hello peeps!{' '}
-            <img className="pic" src={Nat} alt="Natalia Blanco"></img>
-          </h2>
-
-          <p className="section__p">
-            A versatile individual with a large background in the Fashion
-            indsutry, recently converted into the tech world and looking for new
-            opportunities as a junior developer. I put passion, love and effort
-            in what I do. I like to evolve throught times and always improve for
-            the better. In my own personal life I like to spend time with my
-            family, surf when I can, travel, read and learn new stuff .
-          </p>
+          <img className="pic" src={Nat} alt="Natalia Blanco"></img>
+          <section className="text__section">
+            <h2 className="section__subtitle--2">Hello peeps! </h2>
+            <p className="section__p">
+              A versatile individual with a large background in the Fashion
+              indsutry, recently converted into the tech world and looking for
+              new opportunities as a junior developer. I put passion, love and
+              effort in what I do. I like to evolve throught times and always
+              improve for the better. In my own personal life I like to spend
+              time with my family, surf when I can, travel, read and learn new
+              stuff .
+            </p>
+          </section>
         </section>
         <Menu />
       </div>
